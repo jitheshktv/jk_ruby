f29a819d3b5e6b517f58316ff736b4a9af866599
jitheshktv <jitheshktv@gmail.com>
2016-12-18 22:18:13 -0600

    changes

--- BlocksConstantsModules/28Debugging.rb
+++ BlocksConstantsModules/28Debugging.rb
@@ -1,3 +1,9 @@
 # since objects can be converted to strings, logging helps in debugging.
-# Another way is the interactive debugging.
+# Another way is the interactive debugging, by adding breakpoints.
+
+# from commandline, using library debug.rb
+# ruby -r debug runner.rb (runner is the code)
+# next <n> executes the next n lines.
+# p <var> prints the variable var value.
+
 
---
+++ BlocksConstantsModules/29packaging.rb
@@ -0,0 +1,31 @@
+# making your code available for others to run
+
+#1. download from source control, and then bundler to install dependencies and run.
+#2. package the code as gem.
+#    create a new test gem application. It will create the project fodler structure for you.
+#    dependencies are in Gemfile, which in turn points to the gemspec file. This is fine when you gem as a library.
+#    But when you are packaging , we need to specify the dependencies in Gemfile itself.
+#
+# gem install bundler
+# bundle gem test_gem
+# gem build test_gem.gemspec # name of the gemspec file.
+
+# gem install test_gem
+# gem list
+
+# We can create our machine as a source for a gem by running it a a gem server
+  # gem server # this will start the gem server on port 8808
+# now from another machine
+  # gem install test_gem --source <machine_name>:8808
+
+# https://quickleft.com/blog/engineering-lunch-series-step-by-step-guide-to-building-your-first-ruby-gem/
+
+# To provide the users with executables, you can use ocra. Once you package using ocra, it will give you an executable,
+# which you can execute w/o installing Ruby.
+
+###### Resources
+# rubydoc.info
+# rubygems.org
+# ruby-lang.org
+# ruby-toolbox.com, rspec.info, cukes.info
+
